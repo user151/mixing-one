4212010d299fb6b33156159da8b4a0821f13e823
lbragoszewski <lbragoszewski@gmail.com>
2024-06-12 18:19:21 +0200

    Added lucide icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, NgModule  } from '@angular/core';
 import { Router, Event, NavigationEnd, RouterOutlet } from '@angular/router';
 import { IStaticMethods } from 'preline/preline';
+import { LucideAngularModule } from 'lucide-angular';
+
 
 declare global {
   interface Window {
     HSStaticMethods: IStaticMethods;
   }
 }
+
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LucideAngularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { LucideAngularModule, Home, File, Menu } from 'lucide-angular';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    importProvidersFrom(LucideAngularModule.pick({Home, File, Menu}))
+  ]
 };
